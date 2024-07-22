4c4ce00789105eb4d371d3ef17ec1f5256e4f1f3
Jose Luis Fecor Perez <fecor06@hotmail.com>
2024-07-22 17:18:23 -0600

    Add project files

---
+++ AtAdvancedHands1/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtAdvancedHands1
+{
+    public class Person
+    {
+        //class field
+        private String name;
+        private int age;
+        private Boolean canWalk;
+
+        //constructor
+        public Person(String name) {
+            this.name = name;
+        }
+
+        //class property
+        public String GetName()
+        {
+            return name;
+        }
+        //class property
+        public int GetAge()
+        {
+            return age;
+        }
+        //class property
+        public void SetAge(int value)
+        {
+            this.age = value;
+            if (age < 2 || age > 89)
+                canWalk = false;
+            else
+                canWalk = true;
+        }
+        //class property
+        public Boolean GetWalk()
+        {
+            return canWalk;
+        }
+    }
+    }
---
+++ AtAdvancedHands1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace AtAdvancedHands1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Luis");
+            //Person person = new Person("36");
+            //Person person = new Perro("Tyson");
+            try
+            {
+                if (person is Person)
+                {
+                    person.SetAge(36);
+                    if (Equal(person))
+                    {
+                        Console.WriteLine("Se ingresó el mismo nombre y edad para la persona");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"El nombre de la persona es: {person.GetName()}");
+                        Console.WriteLine($"La edad de la persona es: {person.GetAge()}");
+                        Console.WriteLine($"La persona puede caminar? {person.GetWalk()}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("El tipo del objeto no es el correcto");
+            }
+        }
+        static Boolean Equal(Person person)
+        {
+                String strAge = person.GetAge().ToString();
+                    if (person.GetName().Equals(strAge))
+                        return true;
+                    else
+                        return false;
+        }
+    }
+}
